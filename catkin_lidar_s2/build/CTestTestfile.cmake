d04c53153e0cfaa8f51e4268dd40d42e0e46c045
CloudyToSunny <76858453+Cloudytosunny@users.noreply.github.com>
2022-07-15 17:08:05 +0800

    Delete catkin_lidar_s2 directory

--- catkin_lidar_s2/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/wsh/catkin_lidar_s2/src
-# Build directory: /home/ubuntu/wsh/catkin_lidar_s2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("rplidar_ros_s2")
