7a504d62a178f6563c7a9c1f03ddd8a29fcb4137
Cloudytosunny <wangshaohu@seu.edu.cn>
2022-07-15 17:44:21 +0800

    add shell

---
+++ shell/start.sh
@@ -0,0 +1,5 @@
+gnome-terminal -- roscore
+sleep 2s
+gnome-terminal -- sudo chmod 777 /dev/ttyAMA2;sudo chmod 777 /dev/mem;sudo chmod 777 /dev/gpiomem;sudo chmod 777 /dev/ttyAMA0;sudo chmod 777 /dev/ttyAMA1;sudo chmod 777 /dev/ttyAMA3;sudo chmod 777 /dev/ttyUSB0;
+
+
