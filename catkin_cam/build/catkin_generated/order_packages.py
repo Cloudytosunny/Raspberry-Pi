31c90ea803068c75bb5393eb5451c05dce91a7bb
CloudyToSunny <76858453+Cloudytosunny@users.noreply.github.com>
2022-07-15 17:07:49 +0800

    Delete catkin_cam directory

--- catkin_cam/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/ubuntu/wsh/catkin_cam/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/ubuntu/wsh/catkin_TOF/devel;/home/ubuntu/wsh/catkin_ws/devel;/home/ubuntu/wsh/wit/wit_ros_ws/devel;/opt/ros/noetic'.split(';') if '/home/ubuntu/wsh/catkin_TOF/devel;/home/ubuntu/wsh/catkin_ws/devel;/home/ubuntu/wsh/wit/wit_ros_ws/devel;/opt/ros/noetic' != '' else []
--- catkin_cam/build/usb_cam/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/wsh/catkin_cam/src/usb_cam
-# Build directory: /home/ubuntu/wsh/catkin_cam/build/usb_cam
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
